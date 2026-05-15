a2161da6e29eac3f449cae3efcc5b509f3a4e70a
Bennett <69775498+supercat3point0@users.noreply.github.com>
2026-05-14 21:17:54 -0700

    suface

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,13 +1,16 @@
 #include <algorithm>
-#include <array>
+#include <cassert>
 #include <cstdint>
+#include <cstddef>
 #include <exception>
 #include <format>
+#include <ranges>
+#include <tuple>
 #include <utility>
 #include <vector>
 
-#include <vulkan/vulkan_raii.hpp>
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan_raii.hpp>
 
 #include "demo/log.h"
 
@@ -15,6 +18,12 @@ constexpr int width  = 800;
 constexpr int height = 600;
 
 constexpr Severity log_level = Severity::WARN;
+
+static std::vector<const char *> layers    = {};
+static std::vector<const char *> inst_exts = {};
+static std::vector<const char *> dev_exts  = {
+  vk::KHRSwapchainExtensionName
+};
 
 // validation layers are only enabled by default on debug builds
 #ifdef NDEBUG
@@ -39,7 +48,10 @@ private:
   vk::raii::Context                context;
   vk::raii::Instance               instance        = nullptr;
   vk::raii::DebugUtilsMessengerEXT debug_messenger = nullptr;
+  vk::raii::SurfaceKHR             surface         = nullptr;
   vk::raii::PhysicalDevice         physical_device = nullptr;
+  vk::raii::Device                 device          = nullptr;
+  vk::raii::Queue                  queue           = nullptr;
 
   static VKAPI_ATTR vk::Bool32 VKAPI_CALL debug_callback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
                                                          vk::DebugUtilsMessageTypeFlagsEXT type,
@@ -75,12 +87,11 @@ private:
       .apiVersion         = vk::ApiVersion13
     };
 
-    std::vector<const char *> layers;
     if (enable_validation_layers) {
       layers.push_back("VK_LAYER_KHRONOS_validation");
     }
     std::vector<vk::LayerProperties> layer_props = context.enumerateInstanceLayerProperties();
-    std::vector<const char *>::iterator unsupported_layer = std::ranges::find_if(layers, [&layer_props](const char *layer) -> bool {
+    decltype(layers)::iterator unsupported_layer = std::ranges::find_if(layers, [&layer_props](const char *layer) -> bool {
       return std::ranges::none_of(layer_props, [layer](const vk::LayerProperties& layer_prop) -> bool {
         return std::strcmp(layer, layer_prop.layerName) == 0;
       });
@@ -90,25 +101,25 @@ private:
 
     std::uint32_t glfw_num_exts;
     const char **glfw_exts = glfwGetRequiredInstanceExtensions(&glfw_num_exts);
-    std::vector<const char *> exts = std::vector(glfw_exts, glfw_exts + glfw_num_exts);
+    inst_exts.insert(inst_exts.end(), glfw_exts, glfw_exts + glfw_num_exts);
     if (enable_validation_layers) {
-      exts.push_back(vk::EXTDebugUtilsExtensionName);
+      inst_exts.push_back(vk::EXTDebugUtilsExtensionName);
     }
     std::vector<vk::ExtensionProperties> ext_props = context.enumerateInstanceExtensionProperties();
-    std::vector<const char*>::iterator unsupported_ext = std::ranges::find_if(exts, [&ext_props](const char *ext) -> bool {
+    decltype(inst_exts)::iterator unsupported_ext = std::ranges::find_if(inst_exts, [&ext_props](const char *ext) -> bool {
       return std::ranges::none_of(ext_props, [ext](const vk::ExtensionProperties& ext_prop) -> bool {
         return std::strcmp(ext, ext_prop.extensionName);
       });
     });
-    if (unsupported_ext != exts.end())
+    if (unsupported_ext != inst_exts.end())
       throw std::runtime_error(std::format("Requied extension not supported: {}", *unsupported_ext));
 
     vk::InstanceCreateInfo create_info = {
       .pApplicationInfo        = &app_info,
       .enabledLayerCount       = static_cast<std::uint32_t>(layers.size()),
       .ppEnabledLayerNames     = layers.data(),
-      .enabledExtensionCount   = static_cast<std::uint32_t>(exts.size()),
-      .ppEnabledExtensionNames = exts.data()
+      .enabledExtensionCount   = static_cast<std::uint32_t>(inst_exts.size()),
+      .ppEnabledExtensionNames = inst_exts.data()
     };
 
     if (enable_validation_layers) {
@@ -134,38 +145,45 @@ private:
         = vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
         | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
         | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance;
-      vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT> create_info_chain = {
-        create_info,
-        {
-          .messageSeverity = severities,
-          .messageType = types,
-          .pfnUserCallback = &debug_callback
-        }
+      vk::DebugUtilsMessengerCreateInfoEXT messenger_create_info = {
+        .messageSeverity = severities,
+        .messageType     = types,
+        .pfnUserCallback = &debug_callback
       };
-      instance = vk::raii::Instance(context, create_info_chain.get<vk::InstanceCreateInfo>());
-      debug_messenger = instance.createDebugUtilsMessengerEXT(create_info_chain.get<vk::DebugUtilsMessengerCreateInfoEXT>());
+      create_info.pNext = &messenger_create_info;
+      instance = vk::raii::Instance(context, create_info);
+      debug_messenger = instance.createDebugUtilsMessengerEXT(messenger_create_info);
     } else {
       instance = vk::raii::Instance(context, create_info);
     } 
   }
 
+  void create_surface() {
+    VkSurfaceKHR csurface;
+    if (static_cast<vk::Result>(glfwCreateWindowSurface(*instance, window, nullptr, &csurface)) != vk::Result::eSuccess)
+      throw std::runtime_error("Failed to create a window surface");
+    surface = vk::raii::SurfaceKHR(instance, csurface);
+  }
+
   void select_physical_device() {
-    std::vector<const char *> exts = {
-      vk::KHRSwapchainExtensionName
-    };
     std::vector<vk::raii::PhysicalDevice> physical_devices = instance.enumeratePhysicalDevices();
-    std::vector<vk::raii::PhysicalDevice>::iterator first_device
-      = std::ranges::find_if(physical_devices, [&exts](const vk::raii::PhysicalDevice& device) -> bool {
+    decltype(physical_devices)::iterator first_device
+      = std::ranges::find_if(physical_devices, [this](const vk::raii::PhysicalDevice& device) -> bool {
         if (device.getProperties().apiVersion < vk::ApiVersion13) return false;
-        if (std::ranges::none_of(device.getQueueFamilyProperties(), [](const vk::QueueFamilyProperties& qfp) -> bool {
-          return static_cast<bool>(qfp.queueFlags & vk::QueueFlagBits::eGraphics);
-        })) return false;
+
+        if (std::ranges::none_of(device.getQueueFamilyProperties() | std::views::enumerate,
+          [this, &device](std::tuple<std::size_t, vk::QueueFamilyProperties>&& qfp_tuple) -> bool {
+            auto [ qfp_index, qfp ] = qfp_tuple;
+            return qfp.queueFlags & vk::QueueFlagBits::eGraphics && device.getSurfaceSupportKHR(qfp_index, surface);
+          })) return false;
+
         std::vector<vk::ExtensionProperties> ext_props = device.enumerateDeviceExtensionProperties();
-        if (!std::ranges::all_of(exts, [&ext_props](const char *ext) -> bool {
+        if (!std::ranges::all_of(dev_exts, [&ext_props](const char *ext) -> bool {
           return std::ranges::any_of(ext_props, [ext](const vk::ExtensionProperties& ext_prop) -> bool {
             return std::strcmp(ext, ext_prop.extensionName) == 0;
           });
         })) return false;
+
         vk::StructureChain<vk::PhysicalDeviceFeatures2,
                            vk::PhysicalDeviceVulkan13Features,
                            vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT> features
@@ -179,6 +197,42 @@ private:
     physical_device = *first_device;
   }
 
+  void create_logical_device() {
+    std::vector<vk::QueueFamilyProperties> queue_family_props = physical_device.getQueueFamilyProperties();
+    uint32_t queue_family_index = UINT32_MAX;
+    for (auto [ qfp_index, qfp ] : queue_family_props | std::views::enumerate) {
+      if (qfp.queueFlags & vk::QueueFlagBits::eGraphics && physical_device.getSurfaceSupportKHR(qfp_index, *surface)) {
+        queue_family_index = qfp_index;
+        break;
+      }
+    }
+    assert(queue_family_index != UINT32_MAX && "Queue family doesn't support graphics even though it should");
+    float queue_priority = 0.5f;
+    vk::DeviceQueueCreateInfo queue_create_info = {
+      .queueFamilyIndex = queue_family_index,
+      .queueCount       = 1,
+      .pQueuePriorities = &queue_priority
+    };
+
+    vk::StructureChain<vk::PhysicalDeviceFeatures2,
+                       vk::PhysicalDeviceVulkan13Features,
+                       vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT> features = {
+      {},
+      { .dynamicRendering = true },
+      { .extendedDynamicState = true }
+    };
+
+    vk::DeviceCreateInfo create_info = {
+      .pNext                   = &features.get<vk::PhysicalDeviceFeatures2>(),
+      .queueCreateInfoCount    = 1,
+      .pQueueCreateInfos       = &queue_create_info,
+      .enabledExtensionCount   = static_cast<std::uint32_t>(dev_exts.size()),
+      .ppEnabledExtensionNames = dev_exts.data()
+    };
+    device = vk::raii::Device(physical_device, create_info);
+    queue  = vk::raii::Queue(device, queue_family_index, 0);
+  }
+
   void init() {
     glfwInit();
 
@@ -188,7 +242,9 @@ private:
     window = glfwCreateWindow(width, height, "CAT Engine Demo", nullptr, nullptr);
 
     create_vulkan_instance();
+    create_surface();
     select_physical_device();
+    create_logical_device();
   }
 
   void cleanup() {
